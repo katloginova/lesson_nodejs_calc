d0b0e11a9a14626383bfa73b0a42ccb09e020dd3
Katerina <loginovakaty@gmail.com>
2021-07-31 16:33:26 +0300

    lesson node js calculator

---
+++ app.js
@@ -0,0 +1,9 @@
+'use strict';
+
+
+const calculator = require('./calculator');
+
+console.log(calculator.sum(6, 2));
+console.log(calculator.div(6, 2));
+console.log(calculator.mult(6, 2));
+console.log(calculator.sub(6, 2));
---
+++ calculator.js
@@ -0,0 +1,14 @@
+'use strict';
+
+const sum = require('./sum');
+const div = require('./div');
+const mult = require('./mult');
+const sub = require('./sub');
+
+
+module.exports = {
+    sum,
+    div,
+    mult,
+    sub
+};
